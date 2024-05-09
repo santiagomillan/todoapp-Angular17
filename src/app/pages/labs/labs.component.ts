447d0c88ffc672a647e923d93d66bda977bb3f8b
Santiago Millan <santiagomillanpardo@gmail.com>
2024-05-09 00:48:46 -0500

    Feature: Implementando estilos 02

---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css',
+})
+export class LabsComponent {
+  welcome = 'Hola todoapp!';
+  tasks = ['instalar Angular CLI', 'Crear proyecto', 'Crear Componentes'];
+}
