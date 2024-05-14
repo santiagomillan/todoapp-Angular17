49c2c189522f8f026a0eabeba091b632e804d7f8
Santiago Millan <santiagomillanpardo@gmail.com>
2024-05-13 22:56:12 -0500

    Update Tasks 12

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -47,4 +47,18 @@ export class HomeComponent {
       tasks.filter((task, position) => position !== index)
     );
   }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task;
+      });
+    });
+  }
 }
